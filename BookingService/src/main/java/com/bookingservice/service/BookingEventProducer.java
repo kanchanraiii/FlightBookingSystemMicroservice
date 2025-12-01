4e7eabedf075e3e56221cc2937a970c1909da418
kanchanraiii <kanchanrai2307@gmail.com>
2025-12-02 02:06:31 +0530

    resolved: unused import issue highlighted by SonarQube

--- BookingService/src/main/java/com/bookingservice/service/BookingEventProducer.java
+++ BookingService/src/main/java/com/bookingservice/service/BookingEventProducer.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
 
 import com.bookingservice.model.BookingEvent;
