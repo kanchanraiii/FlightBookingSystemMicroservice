07643401d36b6e0319c1f824b68c2d2c7e855bef
Kanchan Rai <kanchanrai2307@gmail.com>
2025-12-02 01:40:33 +0530

    added: eureka server

---
+++ FlightBookingEurekaServer/src/test/java/com/flighteureka/FlightBookingEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flighteureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
